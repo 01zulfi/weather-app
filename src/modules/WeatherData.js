75b20544acf1aedd18a697581fa90bb90f66c464
Zulfiqar Ali <01zulfiqarali@gmail.com>
2021-10-12 18:38:36 +0500

    Add ability to get data in two units

--- src/modules/WeatherData.js
+++ src/modules/WeatherData.js
@@ -1,11 +1,18 @@
 import pubsub from "./Pubsub";
 
+const getAPILink = (cityName, units) => {
+  const APILink = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=${units}&APPID=449b05371154016e4225fe1f885a634e`;
+  return APILink;
+};
+
 const getWeatherData = async (cityName) => {
-  const response = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=449b05371154016e4225fe1f885a634e`,
-  );
-  const weatherData = await response.json();
-  console.log(weatherData);
+  const [metricUnitsPromise, imperialUnitsPromise] = await Promise.all([
+    fetch(getAPILink(cityName, "metric")),
+    fetch(getAPILink(cityName, "imperial")),
+  ]);
+  const dataInMetricUnits = await metricUnitsPromise.json();
+  const dataInImperialUnits = await imperialUnitsPromise.json();
+  console.log(dataInMetricUnits, dataInImperialUnits);
 };
 
 const weatherDataModule = {
